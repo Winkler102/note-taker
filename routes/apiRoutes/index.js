4088f9d1ab1e71f12014540967bb83bb8312da1e
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-05-28 13:04:52 -0500

    able to add new notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,9 +1,28 @@
+const fs = require('fs');
+const path = require('path')
 const router = require('express').Router();
 const notes = require('../../db/db.json');
+
+function createNewNote(body, noteArray) {
+    const note = body;
+    note.id = 2;
+    noteArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify(noteArray, null, 2)
+    );
+    return body;
+};
 
 router.get('/notes', (req, res) => {
     console.log(notes);
     res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+    console.log(req.body);
+    const note = createNewNote(req.body, notes)
+    res.json(note);
 });
 
 module.exports = router;
